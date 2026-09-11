1169666df6cc217b61e3d81e2b6293a26f94a8e1
Michael Centrelli <acentrelli@gmail.com>
2026-09-10 23:30:16 -0400

    tweaks + basic tmux

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,9 +7,6 @@ vim.keymap.set("i", "jj", "<Esc>", { noremap = true, silent = true })
 -- Insert mode abbreviation: pry -> require IEx; IEx.pry
 vim.cmd([[iabbrev pry require IEx; IEx.pry]])
 
--- Normal mode mapping: Ctrl-B -> Ctrl-Z
-vim.keymap.set("n", "<C-B>", "<C-Z>", { noremap = true, silent = true })
-
 -- Normal mode mapping: sq
 vim.keymap.set("n", "sq", ':silent! normal mpea"<Esc>bi"<Esc>`pl', { noremap = true, silent = true })
 
@@ -19,19 +16,13 @@ vim.keymap.set("n", "qs", ":silent! normal mpeld bhd `ph<CR>", { noremap = true,
 -- Normal mode mapping: Ctrl-T toggles NERDTree
 vim.keymap.set("n", "<C-t>", ":NERDTreeToggle<CR>", { noremap = true, silent = true })
 
--- fzf stuff
-vim.keymap.set("n", "<leader>/", ":Ag<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>w", ":Ag <C-R><C-W><CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-P>", ":Files<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-p>", ":Files<CR>", { silent = true })
-vim.keymap.set("n", "<C-S-p>", function()
-  local dir = vim.fn.input("Directory: ", "", "dir")
-  if dir ~= "" then
-    vim.cmd("Files " .. dir)
-  else
-    vim.cmd("Files")
-  end
-end, { silent = true })
+-- Snacks picker
+vim.keymap.set("n", "<leader>/", function() Snacks.picker.grep() end, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>w", function() Snacks.picker.grep_word() end, { noremap = true, silent = true })
+vim.keymap.set("n", "<C-P>", function() Snacks.picker.files() end, { noremap = true, silent = true })
+vim.keymap.set("n", "<C-p>", function() Snacks.picker.files() end, { silent = true })
 
 -- Visual mode: Ctrl-C -> copy selection to system clipboard via pbcopy (macOS)
 vim.keymap.set("v", "<C-c>", ":w !pbcopy<CR><CR>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>mf", ":w | silent !mix format<CR>")
---
+++ nvim/lua/plugins/tmux-navigation.lua
@@ -0,0 +1,16 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  lazy = false,
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+  },
+  keys = {
+    { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
+  },
+}
